be4084b0fd2fcf7a7cb1b7447bcf705da897e593
jujusb <jsantilariob@gmail.com>
2026-06-30 08:00:09 +0000

    Cleanup Caddy configuration and streamline Docker setup by removing unused files and variables

--- run.sh
+++ run.sh
@@ -10,23 +10,6 @@ ensure_network() {
 ensure_network caddy_net
 ensure_network navidrome_internal_net
 
-is_ip() {
-  [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
-}
-
-if [ "$MULTIPLE_CADDY_GLOBAL" = "true" ]; then
-    echo "Multiple global services enabled → using global Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml:docker-compose.caddy.yml"
-else
-    echo "Single global service → using local Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml:docker-compose.caddy.yml:docker-compose.root-caddy.yml"
-    if is_ip "$SERVER_ADDRESS"; then
-        echo "Detected IP → using internal TLS"
-        export TLS_CONFIG='tls internal'
-    else
-        echo "Detected domain → using Cloudflare TLS"
-        export TLS_CONFIG='tls { dns cloudflare {env.CF_API_TOKEN} }'
-    fi
-fi
+export COMPOSE_FILE="docker-compose.yml:docker-compose.caddy.yml"
 docker compose $@
 unset COMPOSE_FILE
