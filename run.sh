5831b75b7c2bec4be01ef76d34894deb04ddd52a
jujusb <jsantilariob@gmail.com>
2026-07-03 07:35:35 +0000

    Refactor Docker Compose configuration and streamline network setup

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-ensure_network() {
-    local net=$1
-    if ! docker network inspect "$net" >/dev/null 2>&1; then
-        echo "Creating network: $net"
-        docker network create "$net"
-    fi
-}
-ensure_network caddy_net
-ensure_network navidrome_internal_net
-
 export COMPOSE_FILE="docker-compose.yml:docker-compose.caddy.yml"
 docker compose $@
 unset COMPOSE_FILE
