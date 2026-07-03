916a06aa76c45ba5ef8dadb0493a6bf65ed2f87f
jujusb <jsantilariob@gmail.com>
2026-07-03 08:10:30 +0000

    Refactor Caddyfile to use service names directly and clean up environment variables in Docker Compose

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+if [[ -z "$CURRENT_SERVICE" ]]; then
+    CURRENT_SERVICE=$(basename "$PWD")
+fi
+export COMPOSE_PROJECT_NAME="$CURRENT_SERVICE"
+export NAVIDROME_DATA_SUBDIR="$CURRENT_SERVICE"
 
 export COMPOSE_FILE="docker-compose.yml:docker-compose.caddy.yml"
 docker compose $@
 unset COMPOSE_FILE
+unset NAVIDROME_DATA_SUBDIR
+unset COMPOSE_PROJECT_NAME
